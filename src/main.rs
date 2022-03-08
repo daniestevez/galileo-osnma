31e0aa174ccac4228d36d68a2cf5fc8d604c6798
Daniel Estévez <daniel@destevez.net>
2022-03-08 22:53:43 +0100

    add main binary
    For the time being, this is a test binary that reads a Galmon
    dump and logs stuff. It should be built with the galmon and env_logger
    features.

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#[cfg(feature = "galmon")]
+use galileo_osnma::bitfields::NmaHeader;
+#[cfg(feature = "galmon")]
+use galileo_osnma::galmon::navmon::nav_mon_message::GalileoInav;
+#[cfg(feature = "galmon")]
+use galileo_osnma::galmon::transport::ReadTransport;
+#[cfg(feature = "galmon")]
+use galileo_osnma::subframe::CollectSubframe;
+
+#[cfg(feature = "galmon")]
+fn main() -> std::io::Result<()> {
+    #[cfg(feature = "env_logger")]
+    env_logger::init();
+    let mut read = ReadTransport::new(std::io::stdin());
+    let mut subframe = CollectSubframe::new();
+    loop {
+        let packet = read.read_packet()?;
+        if let Some(
+            inav @ GalileoInav {
+                reserved1: Some(osnma),
+                ..
+            },
+        ) = &packet.gi
+        {
+            if let Some((hkroot, _mack)) = subframe.feed(
+                osnma[..].try_into().unwrap(),
+                inav.gnss_wn.try_into().unwrap(),
+                inav.gnss_tow,
+                inav.gnss_sv.try_into().unwrap(),
+            ) {
+                let nma_header = &hkroot[..1].try_into().unwrap();
+                let nma_header = NmaHeader(nma_header);
+                dbg!(nma_header);
+            }
+        }
+    }
+}
+
+#[cfg(not(feature = "galmon"))]
+fn main() {}
