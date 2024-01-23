99fdb0761b3dd4b4b9fd7b8243dd5d86f8a3b9a1
Daniel Estévez <daniel@destevez.net>
2024-01-23 09:15:32 +0100

    extract_merkle_tree_root: add argparse

--- utils/extract_merkle_tree_root.py
+++ utils/extract_merkle_tree_root.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 import xml.etree.ElementTree as ET
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='extract_merkle_tree_root.py',
+        description='Extracts the Merkle tree root from an XML file')
+    parser.add_argument('input_file')
+    return parser.parse_args()
+
+
 def main():
-    tree = ET.parse(sys.argv[1])
+    args = parse_args()
+    tree = ET.parse(args.input_file)
     root = tree.getroot()
     for node in root.find('body').find('MerkleTree').findall('TreeNode'):
         if node.find('j').text == '4':
